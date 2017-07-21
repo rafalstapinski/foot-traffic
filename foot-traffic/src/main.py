001507ce176f88687554ff96032e470502ba6306
Rafal Stapinski <stapinskirafal@gmail.com>
2017-07-21 12:45:28 -0400

    add stats

--- foot-traffic/src/main.py
+++ foot-traffic/src/main.py
@@ -3,6 +3,7 @@ from config import *
 import json
 import web
 import sys
+from datetime import datetime
 
 db = web.database(dbn=DB.dbn, db=DB.db)
 
@@ -31,7 +32,7 @@ def first_run(chain_id):
                 if db.select('locations', dict(venue_id=venue['id']), where='id=$venue_id').first() is None:
                     db.insert('locations', id=venue['id'], chain_id=chain_id)
 
-def norm_run(chain_id):
+def run(chain_id):
 
     chain = db.select('chains', dict(id=chain_id), where='id=$id').first()
 
@@ -39,21 +40,39 @@ def norm_run(chain_id):
 
     for location in locations:
 
-        url = API.url % ('venues/search', '&query=%s&ll=%s,%s' % (chain.name, location.lat, location.lng))
-
+        url = API.url % ('venues/%s' % location.id, '')
         r = requests.get(url).json()
+        venue = r['response']['venue']
 
-        for venue in r['response']['venues']:
+        print venue['stats']['checkinsCount'], venue['stats']['usersCount'], venue['stats']['visitsCount'], venue['beenHere']['count'], venue['beenHere']['unconfirmedCount']
 
-            db.insert('traffic', venue_id=venue['id'], traffic=venue['stats']['checkinsCount'], chain_id=chain_id)
+        db.insert('stats',  venue_id=venue['id'],
+                            been_here=venue['beenHere']['count'],
+                            been_here_unc=venue['beenHere']['unconfirmedCount'],
+                            checkins_count=venue['stats']['checkinsCount'],
+                            users_count=venue['stats']['usersCount'],
+                            tip_count=venue['stats']['tipCount'],
+                            visits_count=venue['stats']['visitsCount'],
+                            date=datetime.now()
+                )
+
+
+def run_all():
+
+    chains = db.select('chains').list()
+
+    for chain in chains:
+        run(chain.id)
 
 cmd = sys.argv[1]
 
 if cmd == 'setup_db':
-
     for q in DB.setup_sql:
         db.query(q)
 
 elif cmd == 'add':
     chain_id = add_chain(sys.argv[2])
     first_run(chain_id)
+
+elif cmd == 'run_all':
+    run_all()
